99594a441143f7c04a72d3a4b2355331470c3ed4
soncomqiq <idgamescs@hotmail.com>
2026-05-27 00:05:19 +0700

    fix: Update Spring Boot version and adjust test dependencies

--- backend/src/test/java/com/sonter/exam_protector/ExamProtectorApplicationTests.java
+++ backend/src/test/java/com/sonter/exam_protector/ExamProtectorApplicationTests.java
@@ -1,9 +1,7 @@
 package com.sonter.exam_protector;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ExamProtectorApplicationTests {
 
 	@Test
